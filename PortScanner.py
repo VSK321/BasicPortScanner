31f0502e1e23dd1ee5b2f8a38817111d43379b7a
VSK321 <37224049+VSK321@users.noreply.github.com>
2019-01-22 22:20:42 +0000

    Add files via upload

---
+++ PortScanner.py
@@ -0,0 +1,20 @@
+import socket
+import time
+socket.setdefaulttimeout(0.5)
+
+Target_Server = "192.168.0.3"
+
+start = time.time()
+for x in range(1, 65536):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+    try:
+        sock.connect((Target_Server,x))
+        print("Port "+str(x)+" is open")
+        sock.close()
+        del sock
+    except:
+        print("Port "+str(x)+" is closed")
+        sock.close()
+        del sock
+print(time.time()-start)
+
